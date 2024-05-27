eb19795c963cb0686808bde5fa69372b1c1fd015
Nhàn Nguyễn <t18.arsenal@gmail.com>
2024-05-27 23:36:07 +0700

    api crud hàng hóa

---
+++ MyWebAPI/MyWebAPI/Controllers/HangHoaController.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MyWebAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HangHoaController : ControllerBase
+    {
+        public static List<HangHoa> hangHoas = new List<HangHoa>();
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            try
+            {
+                return Ok(hangHoas);
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Add(HangHoa hanghoa)
+        {
+            try
+            {
+                var hh = new HangHoa
+                {
+                    MaHangHoa = Guid.NewGuid(),
+                    TenHangHoa = hanghoa.TenHangHoa,
+                    DonGia = hanghoa.DonGia
+                };
+                hangHoas.Add(hh);
+                return Ok(hh);
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(string id)
+        {
+            try
+            {
+                var hh = hangHoas.FirstOrDefault(x => x.MaHangHoa == Guid.Parse(id));
+                if (hh is null)
+                    return NotFound();
+
+                return Ok(hh);
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Edit(string id, HangHoa hanghoa)
+        {
+            try
+            {
+                var hh = hangHoas.FirstOrDefault(x => x.MaHangHoa == Guid.Parse(id));
+                if (hh is null)
+                    return NotFound();
+                hh.TenHangHoa = hanghoa.TenHangHoa;
+                hh.DonGia = hanghoa.DonGia;
+                
+                return Ok(hh);
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(string id)
+        {
+            try
+            {
+                var hh = hangHoas.FirstOrDefault(x => x.MaHangHoa == Guid.Parse(id));
+                if (hh is null)
+                    return NotFound();
+                hangHoas.Remove(hh);
+                return Ok(hangHoas);
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+            }
+        }
+    }
+}
---
+++ MyWebAPI/MyWebAPI/Models/HangHoa.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace MyWebAPI.Models
+{
+    public class HangHoa
+    {
+        public Guid MaHangHoa { get; set; }
+        public string TenHangHoa { get; set; }
+        public double DonGia { get; set; }
+    }
+}
